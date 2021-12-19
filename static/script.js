62abe7ea2eadf03dc478aa4070dc71cfa3ae0357
DarkWool <cooper27@live.com.mx>
2021-12-19 17:50:00 -0600

    Added system to check when the game has been finished, won or draw.

--- static/script.js
+++ static/script.js
@@ -1,24 +1,75 @@
 // Module to manage the game flow
-const game = (function (game) {
+const game = (function () {
 	const playerOne = createPlayer("Wool", true, "x");
 	const playerTwo = createPlayer("PC", false, "o");
 
-	function changeTurn() {
-		if (playerOne.turn) {
-			playerOne.turn = false;
-			playerTwo.turn = true;
+	const boardSection = document.getElementById("gameBoard");
+	const gameResult = document.createElement("p");
+	gameResult.classList.add("game-status");
 
-			return playerOne.symbol;
-		} else {
-			playerOne.turn = true;
-			playerTwo.turn = false;
+	function _changeTurn() {
+		playerOne.turn = (playerOne.turn) ? false : true;
+		playerTwo.turn = (playerTwo.turn) ? false : true;
+	}
 
-			return playerTwo.symbol;
+	function getSymbol() {
+		return (playerOne.turn) ? playerOne.symbol : playerTwo.symbol;
+	}
+
+	function _gameWon() {
+		gameResult.textContent = (playerOne.turn) ? `${playerOne.name} has won!` :
+			`${playerTwo.name} has won!`;
+		boardSection.after(gameResult);
+	}
+
+	function _gameDraw() {
+		gameResult.textContent = `Draw.`;
+		boardSection.after(gameResult);
+	}
+
+	function checkStatus(index) {
+		// DRAW
+		(gameBoard.board.every(el => el)) ? _gameDraw() : false;
+
+		// ROWS
+		for (let i = 0; i < 9; i += 3) {
+			if (!gameBoard.board[i]) continue;
+
+			if (gameBoard.board[i] === gameBoard.board[i + 1] &&
+				gameBoard.board[i] === gameBoard.board[i + 2]) {
+				_gameWon();
+				return true;
+			}
 		}
+		
+		// COLUMNS
+		for (let i = 0; i < 3; i++) {
+			if (!gameBoard.board[i]) continue;
+
+			if (gameBoard.board[i] === gameBoard.board[i + 3] &&
+				gameBoard.board[i] === gameBoard.board[i + 6]) {
+				_gameWon();
+				return true;
+			}
+		}
+
+		// DIAGONAL
+		if (index % 2 === 0) {
+			let [topLeft, , topRight, , middle, , bottomLeft, , bottomRight] = gameBoard.board;
+
+			if ((topLeft && topLeft === middle && topLeft === bottomRight) ||
+				(topRight && topRight === middle && topRight === bottomLeft)) {
+				_gameWon();
+				return true;
+			}
+		}
+
+		_changeTurn();
 	}
 
 	return {
-		changeTurn
+		getSymbol,
+		checkStatus
 	}
 })();
 
@@ -26,7 +77,7 @@ const game = (function (game) {
 const gameBoard = (function () {
 	let board = Array(9).fill("");
 
-	// DOM element
+	// DOM elements
 	const boxes = document.getElementById("gameBoard").children;
 	const boxesArray = Array.from(boxes);
 
@@ -41,19 +92,29 @@ const gameBoard = (function () {
 		});
 	}
 
-	function _detachEvents(box) {
+	function _detachSingleEvent(box) {
 		box.removeEventListener("click", _addNewPlay);
 	}
 
+	function _detachEvents() {
+		for (let box of boxes) {
+			box.removeEventListener("click", _addNewPlay)
+		}
+	}
+
 	function _addNewPlay(e) {
+		let symbol = game.getSymbol();
+
 		const box = e.target;
-		let playerSymbol = game.changeTurn();
-
-		if (box.textContent) return;
-		board[boxesArray.indexOf(box)] = playerSymbol;
-
-		_detachEvents(box);
+		let index = boxesArray.indexOf(box);
+		board[index] = symbol;					
 		_render();
+
+		if (game.checkStatus(index)) {
+			return _detachEvents();
+		}
+
+		return _detachSingleEvent(box);
 	}
 
 	_render();
