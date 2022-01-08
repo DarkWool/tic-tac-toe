aad8c16e54aef60f11f7b88b90da8c7613f3913a
DarkWool <cooper27@live.com.mx>
2022-01-08 15:57:30 -0600

    Reworked the code and fixed multiple bugs, 2 players mode is now fully functional with the restart button

--- static/script.js
+++ static/script.js
@@ -1,155 +1,162 @@
-// Module to manage the game flow
 const menu = (function () {
 	const onePlayerBtn = document.getElementById("onePlayer");
 	const twoPlayersBtn = document.getElementById("twoPlayers");
 	const menu = document.getElementById("gameMenu");
 	const game = document.getElementById("game");
 	const newBg = document.getElementsByClassName("green-bg")[0];
-
+	
 	onePlayerBtn.addEventListener("click", startGame);
-
+	
 	function startGame() {
 		menu.remove();
-		game.hidden = false;
-		game.classList.remove("hide-el");
-		game.classList.add("flex-container");
-		game.classList.add("active");
+		game.classList.add("flex-container", "active");
 		newBg.classList.add("active");
 	}
 })();
 
+// Module to manage the game flow
 const game = (function () {
-	const playerOne = createPlayer("Wool", true, "X");
-	const playerTwo = createPlayer("PC", false, "O");
-
-	const boardSection = document.getElementById("gameBoard");
-	const gameResult = document.createElement("p");
-	gameResult.classList.add("game-status");
-
-	function _changeTurn() {
-		playerOne.turn = (playerOne.turn) ? false : true;
-		playerTwo.turn = (playerTwo.turn) ? false : true;
-	}
-
-	function getSymbol() {
-		return (playerOne.turn) ? playerOne.symbol : playerTwo.symbol;
-	}
-
-	function _gameWon() {
-		gameResult.textContent = (playerOne.turn) ? `${playerOne.name} has won!` :
-			`${playerTwo.name} has won!`;
-		boardSection.after(gameResult);
-	}
-
-	function _gameDraw() {
-		gameResult.textContent = `Draw.`;
-		boardSection.after(gameResult);
-	}
-
-	function checkStatus(index) {
-		// DRAW
-		(gameBoard.board.every(el => el)) ? _gameDraw() : false;
-
-		// ROWS
-		for (let i = 0; i < 9; i += 3) {
-			if (!gameBoard.board[i]) continue;
-
-			if (gameBoard.board[i] === gameBoard.board[i + 1] &&
-				gameBoard.board[i] === gameBoard.board[i + 2]) {
-				_gameWon();
-				return true;
-			}
-		}
-		
-		// COLUMNS
-		for (let i = 0; i < 3; i++) {
-			if (!gameBoard.board[i]) continue;
-
-			if (gameBoard.board[i] === gameBoard.board[i + 3] &&
-				gameBoard.board[i] === gameBoard.board[i + 6]) {
-				_gameWon();
-				return true;
-			}
-		}
-
-		// DIAGONAL
-		if (index % 2 === 0) {
-			let [topLeft, , topRight, , middle, , bottomLeft, , bottomRight] = gameBoard.board;
-
-			if ((topLeft && topLeft === middle && topLeft === bottomRight) ||
-				(topRight && topRight === middle && topRight === bottomLeft)) {
-				_gameWon();
-				return true;
-			}
-		}
-
-		_changeTurn();
-	}
-
-	return {
-		getSymbol,
-		checkStatus
-	}
-})();
-
-// Module
-const gameBoard = (function () {
-	let board = Array(9).fill("");
+	// Players
+	const playerOne = createPlayer("Wool", "X");
+	const playerTwo = createPlayer("PC", "O");
 
 	// DOM elements
+	const boardSection = document.getElementById("gameBoard");
+	const gameDisplay = document.getElementById("gameDisplay");
+	const restartBtn = document.getElementById("restartGame");
 	const boxes = document.getElementById("gameBoard").children;
 	const boxesArray = Array.from(boxes);
 
-	// Bind events to game boxes
+	// Variables
+	let turn = "playerOne";
+	let turnsPlayed = 0;
+
+	// Events 
+	restartBtn.addEventListener("click", _restartGame);
 	for (box of boxes) {
-		box.addEventListener("click", _addNewPlay);
+		box.addEventListener("click", _newPlay);
+	}
+
+	// Initial state
+	gameDisplay.textContent = `${playerOne.getMarker()}'s turn`;
+
+	// General functions
+	function _newPlay(e) {
+		turnsPlayed++;
+		const box = e.target;
+		
+		let player = _getPlayer();
+		let marker = player.getMarker();
+		(marker === 'O') ? box.classList.add("second-player") : false;
+		
+		gameBoard.addMarker(boxesArray.indexOf(box), marker);
+		
+		_render();
+		_detachSingleEvent(box);
+
+		// Check if anyone has won when 4 turns have passed,
+		// if there is not any win and the number of turns is 9 it means that its a draw.
+		if (turnsPlayed > 4 && gameBoard.checkForWin()) {
+			_detachAllEvents();
+			return _setWinMessage(marker);
+		} else if (turnsPlayed === 9) {
+			return _setDrawMessage();
+		}
+
+		_changePlayersTurn();
+	}
+
+	function _getPlayer() {
+		return (turn === "playerOne") ? playerOne : playerTwo;
+	}
+
+	function _changePlayersTurn() {
+		if (turn === "playerOne") {
+			turn = "playerTwo";
+			gameDisplay.textContent = `${playerTwo.getMarker()}'s turn`;
+		} else {
+			turn = "playerOne";
+			gameDisplay.textContent = `${playerOne.getMarker()}'s turn`;
+		}
+	}
+	
+	function _setWinMessage(marker) {
+		gameDisplay.textContent = `${marker}'s have won!`;
+	}
+	
+	function _setDrawMessage() {
+		gameDisplay.textContent = "Draw";
+	}
+	
+	function _restartGame() {
+		gameBoard.restartBoard();
+
+		turnsPlayed = 0;
+		turn = "playerOne";
+		gameDisplay.textContent = `${playerOne.getMarker()}'s turn`;
+
+		for (box of boxes) {
+			box.innerHTML = "";
+			box.classList.remove("second-player");
+			box.addEventListener("click", _newPlay);
+		}
 	}
 
 	function _render() {
-		board.forEach((el, index) => {
-			boxes[index].textContent = el;
-		});
+		gameBoard.getBoard().forEach((el, index) => boxes[index].textContent = el);
 	}
 
+	// Event functions
 	function _detachSingleEvent(box) {
-		box.removeEventListener("click", _addNewPlay);
+		box.removeEventListener("click", _newPlay);
 	}
 
-	function _detachEvents() {
-		for (let box of boxes) {
-			box.removeEventListener("click", _addNewPlay)
+	function _detachAllEvents() {
+		for (box of boxes) {
+			box.removeEventListener("click", _newPlay);
 		}
-	}
-
-	function _addNewPlay(e) {
-		const box = e.target;
-		
-		let symbol = game.getSymbol();
-		(symbol === 'O') ? box.classList.add('second-player') : false;
-		
-		let index = boxesArray.indexOf(box);
-		board[index] = symbol;					
-		_render();
-
-		if (game.checkStatus(index)) {
-			return _detachEvents();
-		}
-
-		return _detachSingleEvent(box);
-	}
-
-	_render();
-
-	return {
-		board
 	}
 })();
 
-// Factory Function
-function createPlayer(name, turn, symbol) {
+
+let gameBoard = (function () {
+	let board = Array(9).fill("");
+
+	function addMarker(index, mark) {
+		board[index] = mark;
+	}
+
+	function restartBoard() {
+		board = Array(9).fill("");
+	}
+
+	function checkForWin() {
+		const winConditions = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+		return winConditions.find((curr, index) => {
+			return (board[curr[0]] &&
+				(board[curr[0]] === board[curr[1]] && board[curr[1]] === board[curr[2]]));
+		})
+	}
+
+	function getBoard() {
+		return [...board];
+	}
+
+	return {
+		addMarker,
+		restartBoard,
+		checkForWin,
+		getBoard
+	}
+})();
+
+function createPlayer(name, marker) {
 	return {
 		name,
-		turn,
-		symbol
+		marker,
+		getMarker() {
+			return this.marker;
+		}
 	};
 }
